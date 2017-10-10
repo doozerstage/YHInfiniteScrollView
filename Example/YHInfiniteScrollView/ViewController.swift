72b8d535cd61274e5b9c1661280a577d64d63c55
Yonghwi <yonghwi.nam@gmail.com>
2017-10-10 19:02:20 +0900

    test init with frame

--- Example/YHInfiniteScrollView/ViewController.swift
+++ Example/YHInfiniteScrollView/ViewController.swift
@@ -19,13 +19,45 @@ class ViewController: UIViewController, YHInfiniteScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.addInfiniteView()
+        // Add YHInfiniteScrollView
+//        self.addInfiniteView()
+        print("top layout guide height : \(self.topLayoutGuide.length)")
         
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.addInfiniteViewWithFrame(rect: CGRect(origin: CGPoint(), size: CGSize(width: 300, height: 400)))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        print("top layout guide height : \(self.topLayoutGuide.length)")
+    }
+    
+    
+    // MARK: - YHInfiniteViewDelegate
+    func didScroll(atContentOffsetX: CGFloat) {
+        print("didScroll(atContentOffsetX: CGFloat) : \(atContentOffsetX)")
+    }
+    
+    func willScrollToNextView(atIndex: Int, contentObject: Any) {
+        print("willScrollToNextView(atIndex: \(atIndex), contentObject: \(contentObject)")
+    }
+    
+    func didScrollToNextView(atIndex: Int, contentObject: Any) {
+        print("didScrollToNextView(atIndex: \(atIndex), contentObject: \(contentObject)")
+    }
+    
+    func willScrollToPreviousView(atIndex: Int, contentObject: Any) {
+        print("willScrollToPreviousView(atIndex: \(atIndex), contentObject: \(contentObject)")
+    }
+    
+    func didScrollToPreviousView(atIndex: Int, contentObject: Any) {
+        print("didScrollToPreviousView(atIndex: \(atIndex), contentObject: \(contentObject)")
     }
     
     // MARK: - Private Method
     fileprivate func addInfiniteView() {
-        
+        // Initialize YHInfiniteScrollView with layout constraints
         let infiniteScrollView = YHInfiniteScrollView.init(frame: CGRect.zero, contentObjects: self.contentsViewControllers())
         infiniteScrollView.translatesAutoresizingMaskIntoConstraints = false
         infiniteScrollView.delegate = self
@@ -38,6 +70,12 @@ class ViewController: UIViewController, YHInfiniteScrollViewDelegate {
         let alcTrailing = NSLayoutConstraint.init(item: infiniteScrollView, attribute: .trailing, relatedBy: .equal, toItem: self.mainContainer, attribute: .trailing, multiplier: 1.0, constant: 0.0)
         
         self.mainContainer.addConstraints([alcTop, alcLeading, alcBottom, alcTrailing])
+    }
+    
+    fileprivate func addInfiniteViewWithFrame(rect: CGRect) {
+        let infiniteScrollView = YHInfiniteScrollView.init(frame: rect, contentObjects: self.contentsViewControllers())
+        infiniteScrollView.delegate = self
+        self.mainContainer.addSubview(infiniteScrollView)
     }
 
     
