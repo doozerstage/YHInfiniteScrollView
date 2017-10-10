9eb7e52cfec9813ac5062967714c1b85ba70413a
Yonghwi <yonghwi.nam@gmail.com>
2017-10-10 16:57:27 +0900

    fix lauch screen

---
+++ Example/YHInfiniteScrollView/Helper.swift
@@ -0,0 +1,9 @@
+//
+//  Helper.swift
+//  YHInfiniteScrollView_Example
+//
+//  Created by DEV_TEAM1_IOS on 10/10/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
--- Example/YHInfiniteScrollView/ViewController.swift
+++ Example/YHInfiniteScrollView/ViewController.swift
@@ -2,17 +2,22 @@
 //  ViewController.swift
 //  YHInfiniteScrollView
 //
-//  Created by yonghwinam on 10/10/2017.
-//  Copyright (c) 2017 yonghwinam. All rights reserved.
+//  Created by Yonghwi Nam on 10/10/2017.
+//  Copyright (c) 2017 DooZerStage. All rights reserved.
 //
 
 import UIKit
+import YHInfiniteScrollView
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var mainContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
